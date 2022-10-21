3ae88d427fe5524da57e09a92202d98fdfe50bf2
David <david.programming@protonmail.com>
2022-10-22 00:35:48 +0200

    feat: add Attribution component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import Calculator from './Calculator';
+import Attribution from './Attribution';
 
 const App = () => {
   return (
     <>
       <Calculator />
+      <Attribution />
     </>
   );
 };
