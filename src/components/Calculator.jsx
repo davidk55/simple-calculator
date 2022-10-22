6238ab5018a35ccd0f62ed230ea2e708b4a7df71
David <david.programming@protonmail.com>
2022-10-22 02:39:10 +0200

    feat: evaluation functionality

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,67 +1,137 @@
+import { useState } from 'react';
 const Calculator = () => {
-  const numbers = [];
+  const numberElements = [];
   for (let i = 9; i > 0; i--) {
-    numbers.push(
+    numberElements.push(
       <button
-        className='border p-2 bg-white border-gray-100 hover:text-gray-900 hover:bg-gray-50'
+        onClick={() => addInput(i.toString())}
+        className='border p-1 bg-white border-gray-100 hover:text-gray-900 hover:bg-gray-50'
         key={i}
       >
         {i}
       </button>
     );
   }
+
+  const [calc, setCalc] = useState('0');
+
+  function isNumber(char) {
+    return /[0-9]/.test(char);
+  }
+
+  function addInput(input) {
+    if (input.length > 1) return;
+    if (calc.length > 9) return;
+    if (!isNumber(input) && !isNumber(calc[calc.length - 1])) return;
+    if (!isNumber(input) && calc == '0') return;
+
+    if (calc == '0') setCalc(input.toString());
+    else setCalc((prevCalc) => prevCalc + input);
+  }
+
+  function clearCalc() {
+    setCalc('0');
+  }
+
+  function evalCalc() {
+    setCalc((prevCalc) => +eval(prevCalc).toFixed(5).toString());
+  }
+
+  function toggleSign() {
+    // position before number
+    let pos = calc.length - 1;
+    while (isNumber(calc[pos]) || calc[pos] == '.') {
+      pos--;
+    }
+
+    if (calc[pos] == '+') {
+      setCalc((prevCalc) => {
+        return prevCalc.substring(0, pos) + '-' + prevCalc.substring(pos + 1);
+      });
+    } else if (calc[pos] == '-') {
+      setCalc((prevCalc) => {
+        return prevCalc.substring(0, pos) + '+' + prevCalc.substring(pos + 1);
+      });
+    } else {
+      setCalc((prevCalc) => {
+        return (
+          prevCalc.substring(0, pos + 1) + '+' + prevCalc.substring(pos + 1)
+        );
+      });
+    }
+  }
+
   return (
     <div className='grid grid-cols-4 rounded-2xl grid-flow-row-dense shadow-xl text-slate-600 font-my-font text-2xl m-auto'>
       <p className='col-start-1 col-end-5 text-right text-cyan-50 bg-black p-6 pb-4 rounded-t-2xl text-3xl'>
-        0
+        {calc}
       </p>
-      <button className='bg-gray-200 p-4 hover:bg-gray-300' type='button'>
+      <button
+        onClick={clearCalc}
+        className='bg-gray-200 p-4 hover:bg-gray-300'
+        type='button'
+      >
         C
       </button>
-      <button className='bg-gray-200 p-4 hover:bg-gray-300' type='button'>
+      <button
+        onClick={toggleSign}
+        className='bg-gray-200 p-4 hover:bg-gray-300'
+        type='button'
+      >
         +/-
       </button>
       <button
+        onClick={() => addInput('%')}
         className='border border-1  bg-gray-200 py-4 px-6 hover:bg-gray-300'
         type='button'
       >
         %
       </button>
-      <button className='bg-red-400 p-4 hover:bg-red-500' type='button'>
+      <button
+        onClick={() => addInput('/')}
+        className='bg-red-400 p-4 hover:bg-red-500'
+        type='button'
+      >
         /
       </button>
       <button
+        onClick={() => addInput('*')}
         className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
         type='button'
       >
         x
       </button>
       <button
+        onClick={() => addInput('-')}
         className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
         type='button'
       >
         -
       </button>
       <button
+        onClick={() => addInput('+')}
         className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
         type='button'
       >
         +
       </button>
       <button
+        onClick={evalCalc}
         className='bg-purple-300 col-start-3 col-end-5 p-2 rounded-br-2xl hover:bg-purple-400'
         type='button'
       >
         =
       </button>
-      {numbers}
+      {numberElements}
       <button
+        onClick={() => addInput('0')}
         className='border border-1 p-2 rounded-bl-2xl bg-white hover:bg-gray-50'
         type='button'
       >
         0
       </button>
       <button
+        onClick={() => addInput('.')}
         className='border border-1 p-2 bg-white hover:bg-gray-50'
         type='button'
       >
