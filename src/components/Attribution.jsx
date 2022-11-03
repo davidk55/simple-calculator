e516e591191bcadb805722af45c3a33f0caa380d
David <david.programming@protonmail.com>
2022-11-03 16:23:02 +0100

    refactor: formatting

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,6 +1,6 @@
 const Attribution = () => {
   return (
-    <footer className='bg-white bg-opacity-75 mb-6 py-2  rounded-xl px-8'>
+    <footer className='mb-6 rounded-xl bg-white bg-opacity-75  py-2 px-8'>
       <p className='text-sm tracking-wide'>
         Made by{' '}
         <a className='text-cyan-400' href='https://github.com/davidk55'>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,7 +5,7 @@ const Calculator = () => {
     numberElements.push(
       <button
         onClick={() => addInput(i.toString())}
-        className='border p-1 bg-white border-gray-100 hover:text-gray-900 hover:bg-gray-50'
+        className='border border-gray-100 bg-white p-1 hover:bg-gray-50 hover:text-gray-900'
         key={i}
       >
         {i}
@@ -72,8 +72,8 @@ const Calculator = () => {
   }
 
   return (
-    <div className='grid grid-cols-4 rounded-2xl grid-flow-row-dense shadow-xl text-slate-600 font-my-font text-2xl m-auto'>
-      <p className='col-start-1 col-end-5 text-right text-cyan-50 bg-black p-6 pb-4 rounded-t-2xl text-3xl'>
+    <div className='m-auto grid grid-flow-row-dense grid-cols-4 rounded-2xl font-my-font text-2xl text-slate-600 shadow-xl'>
+      <p className='col-start-1 col-end-5 rounded-t-2xl bg-black p-6 pb-4 text-right text-3xl text-cyan-50'>
         {calc}
       </p>
       <button
@@ -92,7 +92,7 @@ const Calculator = () => {
       </button>
       <button
         onClick={() => addInput('%')}
-        className='border border-1  bg-gray-200 py-4 px-6 hover:bg-gray-300'
+        className='border-1 border  bg-gray-200 py-4 px-6 hover:bg-gray-300'
         type='button'
       >
         %
@@ -106,28 +106,28 @@ const Calculator = () => {
       </button>
       <button
         onClick={() => addInput('*')}
-        className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
+        className='col-start-4 col-end-5 bg-red-400 p-4 hover:bg-red-500'
         type='button'
       >
         x
       </button>
       <button
         onClick={() => addInput('-')}
-        className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
+        className='col-start-4 col-end-5 bg-red-400 p-4 hover:bg-red-500'
         type='button'
       >
         -
       </button>
       <button
         onClick={() => addInput('+')}
-        className='bg-red-400 col-start-4 col-end-5 p-4 hover:bg-red-500'
+        className='col-start-4 col-end-5 bg-red-400 p-4 hover:bg-red-500'
         type='button'
       >
         +
       </button>
       <button
         onClick={evalCalc}
-        className='bg-purple-300 col-start-3 col-end-5 p-2 rounded-br-2xl hover:bg-purple-400'
+        className='col-start-3 col-end-5 rounded-br-2xl bg-purple-300 p-2 hover:bg-purple-400'
         type='button'
       >
         =
@@ -135,14 +135,14 @@ const Calculator = () => {
       {numberElements}
       <button
         onClick={() => addInput('0')}
-        className='border border-1 p-2 rounded-bl-2xl bg-white hover:bg-gray-50'
+        className='border-1 rounded-bl-2xl border bg-white p-2 hover:bg-gray-50'
         type='button'
       >
         0
       </button>
       <button
         onClick={() => addInput('.')}
-        className='border border-1 p-2 bg-white hover:bg-gray-50'
+        className='border-1 border bg-white p-2 hover:bg-gray-50'
         type='button'
       >
         .
